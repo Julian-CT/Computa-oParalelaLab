f34977ed3068ceb7cd760288baa59626288b2362
Julian Teixeira <144359181+Julian-CT@users.noreply.github.com>
2024-03-19 15:13:20 -0300

    Lab02botm.c

--- Lab02/Lab02botm.c
+++ Lab02/Lab02botm.c
@@ -1,75 +1,71 @@
 #include <stdio.h>
+#include <pthread.h>
 #include <stdlib.h>
-#include <omp.h>
 
-// Número de linhas e colunas da matriz
-#define m 100
-#define n 50
+#define M 1000
+#define N 1000
+#define THREAD_COUNT 4
 
-// Função para contar o tempo de execução
-double get_execution_time(double start_time) {
-    return omp_get_wtime() - start_time;
-}
+double A[M][N];
+double x[N];
+double y[M];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-// Função principal
-int main() {
-    // Declaração da matriz A, vetor x e vetor resultante y
-    double A[m][n], x[n], y[m];
-    int i, j;
+void Pth_mat_vect(voidrank) {
+    long my_rank = (long) rank;
+    int local_m = M / THREAD_COUNT;
+    int my_first_row = my_rank * local_m;
+    int my_last_row = (my_rank + 1) * local_m;
 
-    // Inicialização da matriz A
-    printf("Matriz A:\n");
-    double start_time = omp_get_wtime(); // Marca o início da inicialização da matriz A
-    #pragma omp parallel for private(j)
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
-            A[i][j] = i + j + 1;
-            printf("%.2f ", A[i][j]);
-        }
-        printf("\n");
-    }
-    double initialization_time = get_execution_time(start_time); // Calcula o tempo de execução da inicialização
-
-    // Inicialização do vetor x
-    printf("\nVetor x:\n");
-    start_time = omp_get_wtime(); // Marca o início da inicialização do vetor x
-    #pragma omp parallel for
-    for (j = 0; j < n; j++) {
-        x[j] = j + 1;
-        printf("%.2f\n", x[j]);
-    }
-    double vector_initialization_time = get_execution_time(start_time); // Calcula o tempo de execução da inicialização do vetor x
-
-    // Variável para armazenar o número de processadores a serem utilizados
-    int num_processors;
-
-    // Cálculo do produto matriz-vetor
-    printf("\nCalculando o produto matriz-vetor...\n");
-    printf("Quantos processadores você deseja utilizar (1, 2, 4, 6 ou 8)? ");
-    scanf("%d", &num_processors); // Leitura do número de processadores a serem utilizados
-
-    start_time = omp_get_wtime(); // Marca o início do cálculo do produto matriz-vetor
-    omp_set_num_threads(num_processors); // Define o número de threads a serem utilizadas
-    #pragma omp parallel for private(j)
-    for (i = 0; i < m; i++) {
+    for (int i = my_first_row; i < my_last_row; i++) {
         y[i] = 0.0;
-        for (j = 0; j < n; j++) {
+        for (int j = 0; j < N; j++) {
             y[i] += A[i][j] * x[j];
         }
     }
-    double matrix_vector_product_time = get_execution_time(start_time); // Calcula o tempo de execução do cálculo do produto matriz-vetor
 
-    // Impressão do vetor resultante y
-    printf("\nVetor resultante y:\n");
-    for (i = 0; i < m; i++) {
+    return NULL;
+}
+
+void generate_random_matrix(double A[M][N], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            A[i][j] = (double) rand() / RAND_MAX;  // Valor aleatório entre 0 e 1
+        }
+    }
+}
+
+void generate_random_vector(double x[N], int size) {
+    for (int i = 0; i < size; i++) {
+        x[i] = (double) rand() / RAND_MAX;  // Valor aleatório entre 0 e 1
+    }
+}
+
+int main() {
+    pthread_t thread_handles[THREAD_COUNT];
+    long thread;
+    srand((unsigned int) time(NULL));
+
+    // Gerar matriz e vetor aleatórios
+    generate_random_matrix(A, M, N);
+    generate_random_vector(x, N);
+
+    // Criar threads
+    for (thread = 0; thread < THREAD_COUNT; thread++) {
+        pthread_create(&thread_handles[thread], NULL, Pth_mat_vect, (void *) thread);
+    }
+
+    // Aguardar término das threads
+    for (thread = 0; thread < THREAD_COUNT; thread++) {
+        pthread_join(thread_handles[thread], NULL);
+    }
+
+    // Imprimir vetor resultante y
+    printf("Vetor resultante y:\n");
+    for (int i = 0; i < M; i++) {
         printf("%.2f ", y[i]);
     }
     printf("\n");
-
-    // Impressão do tempo de execução
-    printf("\nTempo de execução da inicialização da matriz A: %.6f segundos\n", initialization_time);
-    printf("Tempo de execução da inicialização do vetor x: %.6f segundos\n", vector_initialization_time);
-    printf("Tempo de execução do cálculo do produto matriz-vetor: %.6f segundos\n", matrix_vector_product_time);
 
     return 0;
 }

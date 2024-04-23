580375e1e653ab74a674bbf75b137a535b7554b7
Julian Teixeira <144359181+Julian-CT@users.noreply.github.com>
2024-04-23 08:48:41 -0300

    Update openMP.c

--- lab04/openMP.c
+++ lab04/openMP.c
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <omp.h>
+
+#define NUM_THREADS 4
+#define NUM_SUBINTERVALS 1000000
+#define LOWER_BOUND 0.0
+#define UPPER_BOUND M_PI
+
+double f(double x) {
+    return sin(x);
+}
+
+int main() {
+    double interval_width = (UPPER_BOUND - LOWER_BOUND) / NUM_SUBINTERVALS;
+    double sum = 0.0;
+    
+    #pragma omp parallel for num_threads(NUM_THREADS) reduction(+:sum)
+    for (int i = 0; i < NUM_SUBINTERVALS; ++i) {
+        double x_left = LOWER_BOUND + i * interval_width;
+        double x_right = x_left + interval_width;
+        double height = (f(x_left) + f(x_right)) / 2.0;
+        double area = height * interval_width;
+        sum += area;
+    }
+
+    std::cout << "Aproximação da área sob a curva: " << sum << std::endl;
+    
+    return 0;
+}
 

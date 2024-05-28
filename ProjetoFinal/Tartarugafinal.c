a2a03f23349987842eb405e148a653407354096b
Julian Teixeira <144359181+Julian-CT@users.noreply.github.com>
2024-05-28 14:45:15 -0300

    Update Tartarugafinal.c

--- ProjetoFinal/Tartarugafinal.c
+++ ProjetoFinal/Tartarugafinal.c
@@ -4,16 +4,16 @@
 #include <omp.h> // Inclusão da biblioteca OpenMP para funcionalidades de paralelização
 
 #define NUM_THREADS 8 // Define o número de threads que serão usadas
-#define NUM_TERMS 100 // Define o número de termos da série de Taylor a serem calculados
+#define NUM_TERMS 100000 // Define o número de termos da série de Taylor a serem calculados
 
-double e_total = 0.0; // Variável global para armazenar o valor total de e
+long double e_total = 0.0; // Variável global para armazenar o valor total de e
 
 // Função para calcular o fatorial de um número
-double factorial(int n) {
+long double factorial(int n) {
     if (n == 0 || n == 1) { // Se n for 0 ou 1, retorna 1, pois 0! e 1! são ambos 1
         return 1.0;
     }
-    double fat = 1.0; // Inicializa a variável fat para 1
+    long double fat = 1.0; // Inicializa a variável fat para 1
     for (int i = 2; i <= n; i++) { // Loop de 2 até n
         fat *= i; // Multiplica fat pelo valor atual de i
     }
@@ -27,7 +27,7 @@ void *calcularE(void *thread_id) {
     int inicio = id * termos_por_thread; // Calcula o termo inicial para esta thread
     int fim = (id + 1) * termos_por_thread; // Calcula o termo final para esta thread
 
-    double e_parcial = 0.0; // Variável para armazenar a soma parcial calculada por esta thread
+    long double e_parcial = 0.0; // Variável para armazenar a soma parcial calculada por esta thread
     for (int termo = inicio; termo < fim; termo++) { // Loop através dos termos atribuídos a esta thread
         e_parcial += 1.0 / factorial(termo); // Adiciona 1/factorial(termo) à soma parcial
     }
@@ -53,9 +53,9 @@ int main() {
         pthread_join(threads[i], NULL); // Bloqueia até que a thread especificada termine
     }
 
-    e_total += 0.0;
+    e_total += 1.0; // Adiciona o termo 1/0!
 
-    printf("A tartaruga andou: %.50f\n", e_total); // Imprime o valor total calculado de e com alta precisão
+    printf("A tartaruga andou: %.50Lf\n", e_total); // Imprime o valor total calculado de e com alta precisão
 
     pthread_exit(NULL); // Termina o programa principal
 }
